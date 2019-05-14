520a2a85a6e9919ec4ff20cbc1993dcbab156323
Dilyaver Saliev <powerdigital2010@gmail.com>
2019-05-14 11:19:29 +0300

    add epochta service

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -21,6 +21,7 @@ class LoginController extends Controller
 
     private const SMS_SERVICE_SMS_RU = 'smsru';
     private const SMS_SERVICE_SMSCENTER = 'smscenter';
+    private const SMS_SERVICE_EPOCHTA = 'epochta';
 
     /*
     |--------------------------------------------------------------------------
@@ -74,6 +75,8 @@ class LoginController extends Controller
                 $smsStatus = $this->sendSmsRu($phone, $code);
             } elseif (self::SMS_SERVICE_SMSCENTER === $smsService) {
                 $smsStatus = $this->sendSmsCenter($phone, $code);
+            } elseif (self::SMS_SERVICE_EPOCHTA === $smsService) {
+                $smsStatus = $this->sendEpochta($phone, $code);
             } else {
                 Log::error(sprintf('Неизвестный сервис отправки сообщений: %s', $smsService));
             }
@@ -150,6 +153,63 @@ class LoginController extends Controller
 
             return false !== strpos($body, 'OK');
         } catch (Throwable $e) {
+            Log::error(sprintf('SMS sending error: %s', $e->getMessage()));
+
+            return false;
+        }
+    }
+
+    private function sendEpochta(string $phone, string $message): bool
+    {
+        try {
+            $url = 'http://api.atompark.com/members/sms/xml.php';
+            $username = env('EPOCHTA_USERNAME');
+            $password = env('EPOCHTA_PASSWORD');
+
+            $src = "<?xml version='1.0' encoding='UTF-8'?>
+                <SMS>
+                    <operations>
+                        <operation>SEND</operation>
+                    </operations>
+                    <authentification>
+                        <username>$username</username>
+                        <password>$password</password>
+                    </authentification>
+                    <message>
+                        <sender>hrbrand</sender>
+                        <text>$message</text>
+                    </message>
+                    <numbers>
+                        <number>$phone</number>
+                    </numbers>
+                </SMS>";
+
+            $curl = curl_init();
+            $options = array(
+                CURLOPT_URL => $url,
+                CURLOPT_FOLLOWLOCATION => false,
+                CURLOPT_POST => true,
+                CURLOPT_HEADER => false,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_CONNECTTIMEOUT => 15,
+                CURLOPT_TIMEOUT => 100,
+                CURLOPT_POSTFIELDS => array('XML' => $src),
+            );
+
+            curl_setopt_array($curl, $options);
+
+            $body = curl_exec($curl);
+            Log::info(sprintf('An SMS sent to %s', $body));
+            Log::info(sprintf('Epochta response body: %s', $body));
+
+            if (false === $body) {
+                throw new Exception('Http request failed');
+            }
+
+            curl_close($curl);
+
+            return true;
+        } catch (Exception $e) {
             Log::error(sprintf('SMS sending error: %s', $e->getMessage()));
 
             return false;
